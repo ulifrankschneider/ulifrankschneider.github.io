1ea56d8509ae74b1727b192c26130ad92b27894d
ulifrankschneider <uli.frank.schneider@gmail.com>
2025-01-02 23:00:18 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 document.getElementById('generate-email').addEventListener('click', async () => {
     const topic = document.getElementById('email-topic').value;
+    const securityCode = document.getElementById('security-code').value; // Sicherheitscode vom Benutzer
+
     const response = await fetch('https://ulifrankschneider-github-io.onrender.com/generate-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
